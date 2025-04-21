3610b50e688e210879ca3186b3655e2ddaad18e2
sunil-gudle <sunilgudle7844@gmail.com>
2025-04-21 17:47:15 +0530

    Initial Commit

---
+++ src/data/constants.js
@@ -0,0 +1,254 @@
+export const Bio = {
+  name: "Sunil Gudle",
+  roles: [
+    "Backend Developer",
+  ],
+  description:
+    "A Java Backend Developer with 3+ years of hands-on experience in building scalable and efficient server-side applications using Java and Spring Boot. Proficient in developing RESTful APIs, integrating databases like MongoDB, and implementing caching solutions with Redis. Passionate about writing clean, maintainable code and delivering high-performance backend systems that support dynamic web applications!",
+  github: "https://github.com/sunil-gudle",
+  resume: "https://drive.google.com/file/d/1L6RFxycSdS1lRgO9tKzxcwl-89EkRrvZ/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sunil-gudle/",
+  twitter: "https://x.com/SuNilGudle1",
+  insta: "https://www.instagram.com/sunil_gudle/",
+};
+
+export const skills = [
+
+
+
+  {
+    title: "Backend",
+    skills: [
+
+      {
+        name: "Spring Framework",
+        image: "https://media.licdn.com/dms/image/D4D12AQEICFOK-z0akA/article-cover_image-shrink_720_1280/0/1656604745399?e=2147483647&v=beta&t=-Fjm5QYiJOJNzJ89xGv9VYPNxMNxqIX4jscMWXB6VaM",
+      },
+      {
+        name: "Microservices",
+        image:
+          "https://t4.ftcdn.net/jpg/03/85/29/69/360_F_385296927_b9V4skbl94TzxL8AiVAMGdpp9hFeZat0.jpg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Apache Kafka",
+        image:
+          "https://w1.pngwing.com/pngs/489/830/png-transparent-linkedin-logo-big-data-user-profile-microsoft-access-apache-kafka-computer-management-apache-spark-thumbnail.png",
+      },
+
+      {
+        name: "Hibernate",
+        image:
+          "https://cdn.freebiesupply.com/logos/thumbs/2x/hibernate-logo.png",
+      },
+
+      {
+        name: "Apache Camel",
+        image:
+          "https://i.pinimg.com/originals/00/dc/03/00dc0377822a2698d1575e3cec6a9258.png",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkW5QVyzoVT_A8dA6aG4Y6mf_q64EjDpnpRxm49mvUg&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://static-00.iconduck.com/assets.00/gitlab-icon-1024x942-f30d1qro.png",
+      },
+   
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "Railway.app",
+        image:
+          "https://railway.app/brand/logo-light.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+      {
+        name: "Intellij IDEA",
+        image:
+          "https://cdn.freebiesupply.com/logos/large/2x/intellij-idea-1-logo-svg-vector.svg",
+      },
+      {
+        name: "Offset Explorer 2 (Kafka)",
+        image:
+          "https://static.macupdate.com/products/53532/l/offset-explorer-logo.png?v=1633349541",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+  {
+    id: 0,
+    img: "",
+    role: "It Analyst (Java Backend Developer)",
+    company: "TCS",
+    date: "Jan 2025 - Present",
+    desc: "Currently working on the Cloud Migration for existing Spring Apps and also Upgrading the apps with Latest stacks",
+    skills: [
+      "Java",
+      "Spring boot",
+      "Microservice",
+      "Docker",
+      "Apache Kafka",
+      "CI/CD Jenkins",
+      "Junit",
+      "AWS"
+    ],
+  },
+
+    {
+    id: 1,
+    img: "",
+    role: "Senior Project Engineer (Java Back-End Developer)",
+    company: "Wipro Limited",
+    date: "Oct 2021 - Jan 2025",
+    desc: "Worked on the modernization of mainframe legacy services to Java-spring boot microservices",
+    skills: [
+      "Java",
+      "Spring boot",
+      "Microservice",
+      "Apache Kafka",
+      "CI/CD Bamboo",
+      "Junit"
+      "Bitbucket"
+    ],
+  },
+  {
+    id: 2,
+    img: "",
+    role: "Test Engineer (Automation Testing)",
+    company: "Wipro Limited",
+    date: "Nov 2019 - Sept 2021",
+    desc: "Worked on Automation Testing for REST API",
+    skills: [
+      "Java",
+      "Spring boot",
+      "BDD Cucumber",
+      "API Testing",
+    ],
+
+  },
+  
+];
+
+export const education = [
+
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQGVF2qj__3qtg/company-logo_400_400/0/1646318126580?e=2147483647&v=beta&t=m_pWngulU4pmV7TEjPVS2V6BCiKactu8PRf0FGQoWwk",
+    school: "Oriental College Of technology Bhopal",
+    date: "May 2018 - May 2022",
+    desc: "I have done my graducation in Bachelor's degree in Electronic and Communication Engineering at Oriental College Of technology, Bhopal. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. ",
+    degree: "Bachelor of Technology - BTech, Electronic And Communication Engineering",
+  },
+
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6KtCz4kQ5SEhlbeDe2DO8AuOxdyrW1UmrZKzsnwEfZcmcV0QNJsX2I82DZcfC_Oq_cow&usqp=CAU",
+    school: "Gyan Ganga Internation School Jabalpur",
+    date: "Apr 2017 - Mar 2018",
+    desc: "I completed my class 12 high school education at Gyan Ganga Internation School, Jabalpur, where I studied Science with Computer Science.",
+    degree: "CBSC(XII), Science with Computer",
+  },
+
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6KtCz4kQ5SEhlbeDe2DO8AuOxdyrW1UmrZKzsnwEfZcmcV0QNJsX2I82DZcfC_Oq_cow&usqp=CAU",
+    school: "Gyan Ganga Internation School Jabalpur",
+    date: "Apr 2015 - Mar 2016",
+    desc: "I completed my class 10 education at Gyan Ganga Internation School, Jabalpur, where I studied Science with Computer Application.",
+    degree: "CBSC(X), Science with Computer",
+  },
+
+
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Expense Tracking API",
+    date: "Jan 2025 - Feb 2025",
+    description:
+      "Tried to implement the possible functionalities for expense tracking application in the backend.",
+    image:
+      "",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Hibernate",
+      "Msql",
+    ],
+    category: "Backend API",
+    github: "https://github.com/sunil-gudle/expenseTrackerApi",
+  },
+  {
+    id: 0,
+    title: "Spring Data JPA",
+    date: "Dec 2024 - Feb 2025",
+    description:
+      "Tried to implement CRUD operations using Spring Data JPA",
+    image:
+      "",
+    tags: ["Java", "Spring Boot", "Data JPA", "Hibernate", "MySQL"],
+    category: "Backend API",
+    github: "https://github.com/sunil-gudle/spring-data-jpa-tutorial",
+   
+  },
+  {
+    id: 1,
+    title: "Spring Boot Application",
+    date: "Apr 2024 - Present",
+    description:
+      "Basic Spring Boot Application",
+    image:
+      "",
+    tags: [
+      "Spring Boot",
+      "Java",
+      "Spring Security",
+    ],
+
+    github: "https://github.com/sunil-gudle/spring-boot-tutorial",
+
+  }
+
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my career in Wipro" },
+  { year: 2025, text: "Switched the Organization" },
+];
